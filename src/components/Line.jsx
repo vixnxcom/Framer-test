cdeffdd243c6e12301738991fc154184c489f5b5
alex aku <vixndotcom@gmail.com>
2024-09-30 00:10:12 +0100

    cake

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -3,13 +3,13 @@ import { arrow } from '../assets'
 
 const Line = () => {
   return (
-    <div className='w-full flex justify-center z-[999] bg-white w-[90vw] h-[auto]'>
-      <div className='flex flex-row'>
-        <h1 className='text-yellow-400 text-[6vw] text-center mt-1 p-2'>
+    <div className=' flex justify-center z-[999] bg-white w-[90vw] h-[auto] mx-auto'>
+      <div className='flex flex-row mx-auto'>
+        <h1 className='text-yellow-400 text-[6vw] text-center mt-1 p-2 mx-4'>
             Lets eat Cake
         </h1>
         <button>
-            <img src={arrow} alt="" className='w-[30%] mt-1' />
+            <img src={arrow} alt="" className='w-[35%] p-1' />
         </button>
       </div>
     </div>
