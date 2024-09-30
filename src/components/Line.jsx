0b95973d518891ce4dbf9022127a9bd1a6e3b3be
alex aku <vixndotcom@gmail.com>
2024-09-30 08:56:40 +0100

    neo

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { delay, motion } from 'framer-motion';
 import { arrow } from '../assets';
 
 const Line = () => {
@@ -7,10 +7,10 @@ const Line = () => {
 
 
   return (
-    <motion.div className='flex justify-center z-[999] bg-white w-[90vw] h-auto' 
+    <motion.div className='flex justify-center z-[999] bg-white w-[90vw] h-auto neo mx-4 ' 
     initial={{opacity: 0, x: -100}} 
     animate={{opacity:1, x: 0}} 
-    transition={{scale: [2,1,2,1],  duration: 1, delay: 4 ,stiffness: 800, type: 'spring'}}
+    transition={{scale: [2,1,2,1],  duration: 3, delay: 4 ,stiffness: 800, type: 'spring'}}
     whileHover={{ scale: [1.2, 1], originX: 0,}} >
 
 
@@ -21,14 +21,15 @@ const Line = () => {
           Let's eat Cake
         </h1>
         <motion.button
-          initial={{ scale: 1 }} // Starts at normal size
-          animate={{ scale: [1, 1.2, 0.9, 1] }} // Bounce effect (scales up and down)
+          initial={{opacity: 0, scale: 1 }} // Starts at normal size
+          animate={{opacity: 1,  scale: [1, 1.2, 0.9, 1] }} // Bounce effect (scales up and down)
           transition={{
-            duration: 1.5, // Duration of each loop
+            duration: 1.5, 
+            delay: 5,// Duration of each loop
             ease: "easeInOut", // Smooth easing
             repeat: Infinity, // Repeat forever
             repeatType: "loop", // Loop the animation
-            repeatDelay: 0.5, // Delay between each repeat
+            repeatDelay: 0.6, // Delay between each repeat
           }}
           className='flex items-center justify-center'
         >
