3b5a11e9e01a15f2a4998ba66716b462064cab17
alex aku <vixndotcom@gmail.com>
2024-10-02 15:13:02 +0100

    texxt

--- src/components/EgNav.jsx
+++ src/components/EgNav.jsx
@@ -107,7 +107,7 @@ const EgNav = () => {
             <img src={close} className="w-24" />
             &times;
           </button>
-          <motion.ul className="space-y-8 text-5xl texxtt">
+          <motion.ul className="space-y-8 text-5xl texxt">
             <motion.li variants={itemVariants}>
               <Link
                 to="/home"
